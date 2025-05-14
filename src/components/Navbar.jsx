b3a7a323e54f6c98dc95cfca1d40205d11b1f125
rivka-flig <rivkaflig@gmail.com>
2025-05-14 17:01:41 -0400

    Navigation Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,36 @@
-import { Link } from 'react-router-dom'
-// import styles from './Navbar.module.css'
+// Import the NavLink component from react-router-dom for navigation between routes
+import { NavLink } from 'react-router-dom';
+// Import CSS module styles
+import styles from './Navbar.module.css';
 
+// Define and export the Navbar functional component
 export default function Navbar() {
   return (
-    <nav>
-      {/* className={styles.nav} */}
-      <Link to="/">Home</Link>
-      <Link to="/about">About Color Theory</Link>
+    // Top-level navigation container with CSS module class
+    <nav className={styles.nav}>
+      
+      {/* Logo or title of the application */}
+      <div className={styles.logo}>Color Explorer</div>
+      
+      {/* Container for navigation links */}
+      <div className={styles.links}>
+
+        {/* NavLink automatically adds an "active" class when the route matches */}
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? styles.active : ''}
+        >
+          Home
+        </NavLink>
+        
+        {/* Another NavLink for the About page */}
+        <NavLink 
+          to="/about" 
+          className={({ isActive }) => isActive ? styles.active : ''}
+        >
+          About Color Theory
+        </NavLink>
+      </div>
     </nav>
-  )
+  );
 }
