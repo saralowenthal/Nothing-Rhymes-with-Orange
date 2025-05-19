9949d3e4c3bca8e76f40a8f565328a094fc40873
rivka-flig <rivkaflig@gmail.com>
2025-05-19 18:14:58 -0400

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nothing-Rhymes-with-Orange',
   plugins: [react()],
+  base: '/Nothing-Rhymes-with-Orange/',
   server: {
     proxy: {
       '/colormagic': {
